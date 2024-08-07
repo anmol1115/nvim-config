1975d7be6b66178d2890d7e371ba9c4d970e6736
Anmol <anmolmoudgil@gmail.com>
2024-08-07 22:18:54 +0530

    Added more movements

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,8 +8,18 @@ vim.api.nvim_set_keymap('i', '<C-h>', '<Left>', {noremap = true})
 vim.api.nvim_set_keymap('i', '<C-j>', '<Down>', {noremap = true})
 vim.api.nvim_set_keymap('i', '<C-k>', '<Up>', {noremap = true})
 vim.api.nvim_set_keymap('i', '<C-l>', '<Right>', {noremap = true})
+
+
+vim.api.nvim_set_keymap('i', '<C-w>', '<Esc>ea', {noremap = true})
+vim.api.nvim_set_keymap('i', '<C-b>', '<Esc>bi', {noremap = true})
+
+-- Adding new line in insert mode
 vim.api.nvim_set_keymap('i', '<C-i>', '<Esc>O', {noremap = true})
 vim.api.nvim_set_keymap('i', '<C-o>', '<Esc>A<CR>', {noremap = true})
+
+-- Vertical motion
+vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', {noremap = true})
 
 -- Movement in command mode
 vim.api.nvim_set_keymap('c', '<C-h>', '<Left>', {noremap = true})
