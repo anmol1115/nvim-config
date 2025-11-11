83de332ca91047116f4d137fb746fecdc69f9c4c
Anmol <anmolmoudgil@gmail.com>
2025-11-11 22:53:06 +0530

    Improved on terminal color change logic

--- lua/plugins/styler.lua
+++ lua/plugins/styler.lua
@@ -12,9 +12,13 @@ return {
       },
     })
 
-    vim.api.nvim_create_autocmd("TermOpen", {
-      callback = function()
-        styler.set_theme(0, { colorscheme = "tokyonight-storm", background = "dark" })
+    vim.api.nvim_create_autocmd({ "TermOpen", "BufWinEnter" }, {
+      callback = function(args)
+        local buf = args.buf
+        if vim.bo[buf].buftype == "terminal" then
+          ---@diagnostic disable-next-line: param-type-mismatch
+          styler.set_theme(0, { colorscheme = "tokyonight-storm", background = "dark" })
+        end
       end,
     })
   end,
